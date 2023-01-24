940771b021ee36c6fcc525fd8e97cd3aa386f6ab
JustinCrowson <Justinwcrowson@gmail.com>
2023-01-24 17:58:13 -0600

    Changes to be committed: 	modified:   assets/script.js 	modified:   index.html

--- assets/script.js
+++ assets/script.js
@@ -20,12 +20,12 @@ locationBtn.addEventListener("click", () =>{
     }
 });
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    api = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
     fetchData();
 }
 function onSuccess(position){
     const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    api = `api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`;
     fetchData();
 }
 function onError(error){
